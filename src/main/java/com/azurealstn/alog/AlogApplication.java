0d32097c0a8b0e75cf7b9c6f074b58f6bba9af90
azurealstn <azurealstn@naver.com>
2022-10-10 19:11:44 +0900

    feat: add BaseTimeEntity JPA Auditing 생성시간,수정시간 공통컬럼은 JPA Auditing을 사용하여 관리

--- src/main/java/com/azurealstn/alog/AlogApplication.java
+++ src/main/java/com/azurealstn/alog/AlogApplication.java
@@ -2,6 +2,7 @@ package com.azurealstn.alog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 public class AlogApplication {
--- src/main/java/com/azurealstn/alog/domain/posts/Posts.java
+++ src/main/java/com/azurealstn/alog/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.azurealstn.alog.domain.posts;
 
+import com.azurealstn.alog.domain.BaseTimeEntity;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,7 +11,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
